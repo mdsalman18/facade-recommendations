979df13f1537d2796884383a060933c226697805
Salman Mohammed <157896817+mdsalman18@users.noreply.github.com>
2025-12-05 18:24:14 +0530

    Delete bash.sh

--- bash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install system dependencies (for building C extensions)
-apt-get update
-apt-get install -y gcc g++ libomp-dev ninja
-
-# Install Python dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
